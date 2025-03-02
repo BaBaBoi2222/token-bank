a14fad0847df5a9f8a1739bc180d827215b766a0
BaBaBoi <117912012+BaBaBoi2222@users.noreply.github.com>
2025-03-03 02:39:09 +0530

    frontend updated
    sign in sign up fully functional

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import SignIn from './sign-in/SignIn.js'
+import SignUp from './sign-up/SignUp.js'
+import RefreshHandler from './RefreshHandler';
+import { useState } from 'react';
+function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const PrivateRoute = ({ element }) => {
+    return isAuthenticated ? element : <Navigate to="/login" />
+  }
+  return (
+    <div className="App">
+      <Router> {/* Wrap everything inside Router */}
+        <RefreshHandler setIsAuthenticated={setIsAuthenticated} />
+        <Routes>
+          <Route path="/" element={<Navigate to="/signin" />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          {/* <Route path='/home' element={<PrivateRoute element={<Home />} />} /> */}
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
