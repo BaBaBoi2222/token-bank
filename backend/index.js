a530b5ecb6db87fbef5e32b9a0a1ed301f112310
BaBaBoi <117912012+BaBaBoi2222@users.noreply.github.com>
2025-03-02 22:51:12 +0530

    backend
    Created backend

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const bodyParser = require('body-parser');
+require('./Models/db');
+const cors = require('cors');
+const PORT = process.env.PORT || 8080;
+const myRouter = require('./Routes/myRouter')
+app.get('/', (req, res)=>{
+    res.send('hi');
+})
+app.use(bodyParser.json());
+app.use(cors())
+app.use('/auth', myRouter)
+app.listen(PORT, ()=> {
+    console.log('Server is running on localhost:'+ PORT.toString());
+    
+})
