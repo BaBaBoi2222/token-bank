1055d74ff6be3bdf2df6c219e64f1ddba6d5ef2a
BaBaBoi <117912012+BaBaBoi2222@users.noreply.github.com>
2025-03-02 22:53:29 +0530

    Formatted
    format correction

--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,13 @@ require('./Models/db');
 const cors = require('cors');
 const PORT = process.env.PORT || 8080;
 const myRouter = require('./Routes/myRouter')
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('hi');
 })
 app.use(bodyParser.json());
 app.use(cors())
 app.use('/auth', myRouter)
-app.listen(PORT, ()=> {
-    console.log('Server is running on localhost:'+ PORT.toString());
-    
+app.listen(PORT, () => {
+    console.log('Server is running on localhost:' + PORT.toString());
+
 })
