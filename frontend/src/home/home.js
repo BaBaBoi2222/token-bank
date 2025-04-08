35ca8c9667f4770f39291a0ba671087dd2b39e41
BaBaBoi <117912012+BaBaBoi2222@users.noreply.github.com>
2025-04-08 22:32:08 +0530

    Update home.js

--- frontend/src/home/home.js
+++ frontend/src/home/home.js
@@ -2,9 +2,9 @@ import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import AppTheme from '../shared-theme/AppTheme';
-import AppAppBar from './components/AppAppBar';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import AppAppBar from '../Home/components/AppAppBar';
+import MainContent from '../Home/components/MainContent';
+import Footer from '../Home/components/Footer';
 export default function Home(props) {
   return (
     <AppTheme {...props}>
